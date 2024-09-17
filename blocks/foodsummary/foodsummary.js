b0e1c8f20da4cc2fd83b9ef4cba9123aaade3d5a
Linus Kohler <linus.kohler@one-inside.com>
2024-09-17 13:58:04 +0200

    Styling improvements of foodmeter

--- blocks/foodsummary/foodsummary.js
+++ blocks/foodsummary/foodsummary.js
@@ -1,1 +1,9 @@
+export default function decorate(block) {
+  const impactLabel = block.querySelector('p');
+  const impactCount = document.createElement('p');
 
+  impactLabel.className = 'foodsummary-label';
+  impactCount.className = 'foodsummary-count';
+
+  impactLabel.parentElement.appendChild(impactCount);
+}
