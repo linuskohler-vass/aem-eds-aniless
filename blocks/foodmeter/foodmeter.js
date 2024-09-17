dbc9935d55fab21b597518b3599f7f30c3ed2316
Linus Kohler <linus.kohler@one-inside.com>
2024-09-17 14:27:57 +0200

    Allow selection of food items

--- blocks/foodmeter/foodmeter.js
+++ blocks/foodmeter/foodmeter.js
@@ -7,18 +7,26 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.className = 'food';
+    li.title = 'Select or deselect food';
+
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     li.children[0].className = 'food__image';
     li.children[1].className = 'food__body';
     li.children[2].className = 'food__impact';
     ul.append(li);
+
+    li.addEventListener('click', (event) => {
+      event.currentTarget.classList.toggle('food-selected');
+    });
   });
+
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
   });
+
   block.textContent = '';
   block.append(ul);
 }
