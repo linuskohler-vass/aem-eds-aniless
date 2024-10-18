f8d35ed58979f859ea0624568f8a1a1dd40cb1d2
Linus Kohler <linus.kohler@one-inside.com>
2024-10-18 14:42:16 +0200

    Added logic for teaser fragment component: A component add takes page content and substracts it to a teaser

--- blocks/teaser-fragment/teaser-fragment.js
+++ blocks/teaser-fragment/teaser-fragment.js
@@ -1,17 +1,37 @@
 import { loadFragment } from '../fragment/fragment.js';
 
 export default async function decorate(block) {
+  const teaserLink = document.createElement('a');
+  teaserLink.href = block.children[1].querySelector('a').href;
+  teaserLink.textContent = block.children[2].querySelector('p').innerHTML;
+  teaserLink.classList.add('button');
+
   const link = block.querySelector('a');
   const path = link ? link.getAttribute('href') : block.textContent.trim();
   const pageFragment = await loadFragment(path);
   if (pageFragment) {
     const pageFragmentSection = pageFragment.querySelector(':scope .section');
+
     if (pageFragmentSection) {
-      const pageFragmentParent = block.closest('.page-fragment');
-      pageFragmentParent.innerHTML = '';
-      [...pageFragment.childNodes].forEach(
-        (childNode) => pageFragmentParent.appendChild(childNode),
-      );
+      const teaserFragmentParent = block.closest('.teaser-fragment');
+      const heroContainer = pageFragment.querySelector('.hero-container');
+      const columnsContainer = pageFragment.querySelector('.columns-container');
+
+      const pictureElement = heroContainer ? heroContainer.querySelector('picture') : null;
+      const h1Element = columnsContainer ? columnsContainer.querySelector('h1') : null;
+
+      // Clear out the existing content in the page-fragment
+      teaserFragmentParent.innerHTML = '';
+
+      if (pictureElement) {
+        teaserFragmentParent.appendChild(pictureElement);
+      }
+
+      if (h1Element) {
+        teaserFragmentParent.appendChild(h1Element);
+      }
+
+      teaserFragmentParent.appendChild(teaserLink);
     }
   }
 }
