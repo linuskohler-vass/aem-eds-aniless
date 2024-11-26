e5a68778755b454fddb1c2a134d1312a952e7bc7
zoranpushev <zoran.pushev@one-inside.com>
2024-11-26 14:49:00 +0100

    Form component model change

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -79,10 +79,9 @@ async function handleSubmit(form) {
 
 export default async function decorate(block) {
   const links = [...block.querySelectorAll('a')].map((a) => a.href);
-  console.log(links);
   const formLink = links.find((link) => link.startsWith(window.location.origin) && link.endsWith('.json'));
   const submitLink = links.find((link) => link !== formLink);
-  // if (!formLink || !submitLink) return;
+  if (!formLink || !submitLink) return;  
 
   const form = await createForm(formLink, submitLink);
   block.replaceChildren(form);
